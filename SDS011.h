9810d8426fcc1e26001538b1b6ff12075fa25644
ubergesundheit <ubergesundheit@googlemail.com>
2017-02-19 10:50:44 +0100

    Change to configurable Serial, Bump Version

--- SDS011.h
+++ SDS011.h
@@ -19,12 +19,10 @@
 
 class SDS011 {
 	public:
-		SDS011(void);
-		void begin(uint8_t pin_rx, uint8_t pin_tx);
+		SDS011(Stream& serial);
 		int read(float *p25, float *p10);
 		void sleep();
 		void wakeup();
 	private:
-		uint8_t _pin_rx, _pin_tx;
-		Stream *sds_data;		
+		Stream& sds_data;
 };
